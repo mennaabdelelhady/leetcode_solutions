f8e6705ced8651fac2026d08aa82acbcb0d6b03e
Menna Abd El-Hady <77082822+mennaabdelelhady@users.noreply.github.com>
2026-04-06 19:23:05 +0200

    Time: 0 ms (100%), Space: 23.7 MB (81.81%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int insertPos = 0;
+        
+        // Move all non-zero elements forward
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[insertPos++] = nums[i];
+            }
+        }
+        
+        // Fill remaining positions with zeros
+        while (insertPos < nums.size()) {
+            nums[insertPos++] = 0;
+        }
+    }
+};
